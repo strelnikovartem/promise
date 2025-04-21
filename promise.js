d1c0cfa855673c27b0800c93ade1b9ca7a5de4a5
strelnikovartem <piramida1237@gmail.com>
2025-04-21 10:38:23 +0000

    Deploying to gh-pages from @ strelnikovartem/promise@be54338367c0ac0a5ccc0af1dba88371a8fbc2c1 🚀

---
+++ promise.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const l=new Promise((o,n)=>{Math.random()>.5&&o("ok"),n("oops")});l.then(o=>{console.log(o)}).catch(o=>{console.log(o)}).finally(()=>{console.log("finally")});
+//# sourceMappingURL=promise.js.map
